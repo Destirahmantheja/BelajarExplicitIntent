e3ad08aebbe840498ef73f95f0b147fbc86ef8c2
Destirahmantheja <destitheja43742@gmail.com>
2022-10-12 10:20:44 +0700

    explicit intent

---
+++ belajarexplitintent/app/src/main/java/com/si5b/myapplication/activitysecond.java
@@ -0,0 +1,13 @@
+package com.si5b.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activitysecond extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
